2400b52d67ac62c5cd2134c3bf1312b169857836
Stanko Horvat <horvat.stanko@gmail.com>
2025-06-24 12:49:42 +0200

    some changes

---
+++ projects/brotli-viewer/src/lib/brotli-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-brotli-viewer',
+  imports: [],
+  template: `
+    <p>
+      brotli-viewer works!
+    </p>
+  `,
+  styles: ``
+})
+export class BrotliViewerComponent {
+
+}
